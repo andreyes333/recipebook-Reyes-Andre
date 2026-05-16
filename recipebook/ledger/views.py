b925fd9c05d775f05458d913af259253673d5df9
Andre Reyes <andrejeremyreyes@gmail.com>
2026-05-16 23:49:00 +0800

    made multiple edits

--- recipebook/ledger/views.py
+++ recipebook/ledger/views.py
@@ -4,10 +4,10 @@ recipes = {
     "recipe1": {
         "name": "Recipe 1",
         "ingredients": [
-            {"name": "tomato", "quantity": "3pcs"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "pork", "quantity": "1kg"},
-            {"name": "water", "quantity": "1L"},
+            {"name": "tomato", "quantity": "3 pcs"},
+            {"name": "onion", "quantity": "1 pc"},
+            {"name": "pork", "quantity": "1 kg"},
+            {"name": "water", "quantity": "1 L"},
             {"name": "sinigang mix", "quantity": "1 packet"}
         ],
     },
@@ -15,8 +15,8 @@ recipes = {
         "name": "Recipe 2",
         "ingredients": [
             {"name": "garlic", "quantity": "1 head"},
-            {"name": "onion", "quantity": "1pc"},
-            {"name": "vinegar", "quantity": "1/2cup"},
+            {"name": "onion", "quantity": "1 pc"},
+            {"name": "vinegar", "quantity": "1/2 cup"},
             {"name": "water", "quantity": "1 cup"},
             {"name": "salt", "quantity": "1 tablespoon"},
             {"name": "whole black peppers", "quantity": "1 tablespoon"},
@@ -28,8 +28,8 @@ recipes = {
 def recipe_list(request):
     ctx = {
         "recipes": [
-            {"name": "Recipe 1", "link": "/recipe/1"}
-            {"name": "Recipe 2", "link": "/recipe/2"}
+            {"name": "Recipe 1", "link": "/recipe/1"},
+            {"name": "Recipe 2", "link": "/recipe/2"},
         ]
     }
     return render(request, 'recipe_list.html', ctx)
