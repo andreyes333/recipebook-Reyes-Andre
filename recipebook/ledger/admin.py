baaa24a847223ad793f952621269bd099f2e3300
Andre Reyes <andrejeremyreyes@gmail.com>
2026-05-18 13:32:50 +0800

    updated admin with RecipeAdmin and RecipeIngredientInline

---
+++ recipebook/ledger/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Recipe, RecipeIngredient
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+
+class RecipeAdmin(admin.ModelAdmin):
+    model = Recipe
+    list_display = ("name",)
+    search_fields = ("name",)
+    inlines = [RecipeIngredientInline,]
+
+admin.site.register(Recipe, RecipeAdmin)
