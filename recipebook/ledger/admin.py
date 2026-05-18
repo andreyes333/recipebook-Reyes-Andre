4f11a09e4b1cd759db5e0e97db2f2a9311a46fb6
Andre Reyes <andrejeremyreyes@gmail.com>
2026-05-18 21:54:18 +0800

    added necessary profile model admins

--- recipebook/ledger/admin.py
+++ recipebook/ledger/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, RecipeIngredient
+from .models import Recipe, Ingredient, RecipeIngredient, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 
 class RecipeIngredientInline(admin.TabularInline):
@@ -21,5 +23,16 @@ class IngredientAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class UserAdmin(admin.BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
