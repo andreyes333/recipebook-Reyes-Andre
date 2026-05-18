0177d4ff234ffba724c8ce282d185cce977921a5
Andre Reyes <andrejeremyreyes@gmail.com>
2026-05-18 22:15:20 +0800

    fixed profile model

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -1,12 +1,23 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
-    bio = models.CharField(max_length=255)
+    bio = models.TextField(
+        validators=[
+            MinLengthValidator(
+                256,
+                'Field must contain more than 255 characters'
+            )
+        ]
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Ingredient(models.Model):
@@ -21,7 +32,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         Profile,
         on_delete=models.CASCADE,
-        related_name="name", #tentative
+        related_name="authors",
     )
     created_on = models.DateTimeField(auto_now_add=True),
     updated_on = models.DateTimeField(auto_now=True),
@@ -41,7 +52,9 @@ class RecipeIngredient(models.Model):
         related_name="recipe",
     )
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name="ingredients"
+        Recipe, 
+        on_delete=models.CASCADE, 
+        related_name="ingredients"
     )
 
     def __str__(self):
