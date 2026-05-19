503443aef18c09cda76e494c4c274147bf5b4592
Andre Reyes <andrejeremyreyes@gmail.com>
2026-05-19 17:10:07 +0800

    made some fixes to models.py

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -67,5 +67,10 @@ class RecipeImage(models.Model):
     )
     description = models.CharField(max_length=255)
     recipe = models.ForeignKey(
-        Recipe, on_delete=models.CASCADE, related_name="ingredients"
+        Recipe, 
+        on_delete=models.CASCADE, 
+        related_name="images"
     )
+
+    def __str__(self):
+        return self.description
